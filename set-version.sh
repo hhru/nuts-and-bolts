40a37c333f396fdd89d1a426c1674f4cc224c6d2
Eli Epifanov <elijah.epifanov@gmail.com>
2010-09-27 16:36:03 +0400

    fixed ./set-version.sh script (s/internal-nab-service/nus-and-bolts/)

--- set-version.sh
+++ set-version.sh
@@ -2,6 +2,6 @@
 
 VERSION=$1
 SED_SCRIPT="s|<version>.*</version><!-- VERSION -->|<version>${VERSION}</version><!-- VERSION -->|g"
-POMS="pom.xml internal-service-nab/pom.xml"
+POMS="pom.xml nuts-and-bolts/pom.xml"
 
 sed -i -e "$SED_SCRIPT" -- $POMS

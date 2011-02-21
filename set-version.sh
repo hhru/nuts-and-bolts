fdf943e98e5cfb6ec07a4c77279facdee6f1bc4a
Eli Epifanov <elijah.epifanov@gmail.com>
2011-02-21 16:50:13 +0300

    made set-version.sh script Mac compatible * HH-18324

--- set-version.sh
+++ set-version.sh
@@ -4,4 +4,4 @@ VERSION=$1
 SED_SCRIPT="s|<version>.*</version><!-- VERSION -->|<version>${VERSION}</version><!-- VERSION -->|g"
 POMS="pom.xml nuts-and-bolts/pom.xml"
 
-sed -i -e "$SED_SCRIPT" -- $POMS
+sed -i "" -e "$SED_SCRIPT" $POMS

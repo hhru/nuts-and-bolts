9d500de0f9471b0cbd8de554242bfd590bfa7f05
Eli Epifanov <elijah.epifanov@gmail.com>
2011-04-08 13:08:29 +0400

    changed application/octet-stream to text/plain in case of 1 byte stub response * HH-19056

--- nuts-and-bolts/src/main/java/ru/hh/nab/jersey/NginxQuirksFilter.java
+++ nuts-and-bolts/src/main/java/ru/hh/nab/jersey/NginxQuirksFilter.java
@@ -5,6 +5,7 @@ import com.sun.jersey.spi.container.ContainerRequestFilter;
 import com.sun.jersey.spi.container.ContainerResponse;
 import com.sun.jersey.spi.container.ContainerResponseFilter;
 import com.sun.jersey.spi.container.ResourceFilter;
+import javax.ws.rs.core.HttpHeaders;
 
 public class NginxQuirksFilter implements ResourceFilter {
   public static class NginxQuirksResponseFilter implements ContainerResponseFilter {
@@ -12,6 +13,7 @@ public class NginxQuirksFilter implements ResourceFilter {
     public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
       if (response.getEntity() == null) {
         response.setEntity(" ".getBytes());
+        response.getHttpHeaders().putSingle(HttpHeaders.CONTENT_TYPE, "text/plain");
       }
       return response;
     }

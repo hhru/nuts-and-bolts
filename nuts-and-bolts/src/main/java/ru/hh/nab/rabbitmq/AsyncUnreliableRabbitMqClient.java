8cd9bd9130021ad2e09e6340f4eaa80e5dbb4522
Denis Borovikov <d.borovikov@hh.ru>
2011-04-21 15:17:19 +0400

    async mq client * HH-18922

---
+++ nuts-and-bolts/src/main/java/ru/hh/nab/rabbitmq/AsyncUnreliableRabbitMqClient.java
@@ -0,0 +1,71 @@
+package ru.hh.nab.rabbitmq;
+
+import com.google.inject.Inject;
+import org.apache.commons.collections.buffer.CircularFifoBuffer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AsyncUnreliableRabbitMqClient extends Thread {
+
+  private final Logger log = LoggerFactory.getLogger(AsyncUnreliableRabbitMqClient.class);
+
+  private final int BUF_SIZE = 1000;
+
+  private final CircularFifoBuffer buffer = new CircularFifoBuffer(BUF_SIZE);
+  private final UnreliableRabbitMqClient mqClient;
+  private final Object mutex = new Object();
+
+  @Inject
+  public AsyncUnreliableRabbitMqClient(UnreliableRabbitMqClient mqClient) {
+    this.mqClient = mqClient;
+  }
+
+  private boolean isBufferEmpty() {
+    synchronized (buffer) {
+      return buffer.isEmpty();
+    }
+  }
+
+  private ChannelAction bufferRemove() {
+    synchronized (buffer) {
+      return (ChannelAction) buffer.remove();
+    }
+  }
+
+  private void bufferAdd(ChannelAction action) {
+    synchronized (buffer) {
+      if (buffer.isFull())
+        log.warn("Buffer overflow");
+      buffer.add(action);
+    }
+  }
+
+  @Override
+  public void run() {
+    while (!isInterrupted()) {
+      synchronized (mutex) {
+        while (isBufferEmpty()) {
+          try {
+            mutex.wait();
+          } catch (InterruptedException e) {
+            interrupt();
+            return;
+          }
+        }
+      }
+      try {
+        mqClient.maybePerform(bufferRemove());
+      } catch (Exception e) {
+        log.error("Can't perform action", e);
+      }
+    }
+  }
+
+  public void maybePerformAsync(ChannelAction action) {
+    synchronized (mutex) {
+      bufferAdd(action);
+      mutex.notify();
+    }
+  }
+}
+

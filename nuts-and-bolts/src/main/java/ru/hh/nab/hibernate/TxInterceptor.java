b23f74a9b3d1828e0ee3e03a2abd75686326e62f
Yuri Ushakov <ushakov@hh.ru>
2012-10-29 19:17:21 +0400

    HH-29452: field renames, simplify if, nested class move.

--- nuts-and-bolts/src/main/java/ru/hh/nab/hibernate/TxInterceptor.java
+++ nuts-and-bolts/src/main/java/ru/hh/nab/hibernate/TxInterceptor.java
@@ -11,80 +11,8 @@ import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
 public class TxInterceptor implements MethodInterceptor {
-  private static class CurrentTx {
-    private final EntityManager em;
-    private boolean readOnly = false;
-    private EntityTransaction tx = null; // null for dummy transactions
-    private final PostCommitHooks postCommitHooks = new PostCommitHooks();
 
-    public CurrentTx(EntityManager em, Transactional ann) {
-      this.em = em;
-      initTx(em, ann);
-    }
-
-    private void initTx(EntityManager em, Transactional ann) {
-      if (!ann.dummy()) {
-        readOnly = ann.readOnly();
-        tx = em.getTransaction();
-        em.setFlushMode(readOnly ? FlushModeType.COMMIT : FlushModeType.AUTO);
-        if (ann.rollback()) {
-          tx.setRollbackOnly();
-        }
-      } else {
-        initDummy();
-      }
-    }
-
-    public void enter(Transactional ann) {
-      if (isDummy()) {
-        initTx(em, ann);
-      } else {
-        if (!tx.getRollbackOnly() && ann.rollback()) {
-          throw new IllegalStateException("Can't execute (rollback() == true) tx while in (rollback() == false) tx");
-        }
-        if (readOnly && !ann.readOnly()) {
-          em.setFlushMode(FlushModeType.AUTO);
-          readOnly = false;
-        }
-      }
-    }
-
-    public void begin() {
-      if (isDummy()) {
-        throw new IllegalStateException("begin() must not be called in a dummy transaction");
-      }
-      tx.begin();
-    }
-
-    public void commit() {
-      if (isDummy()) {
-        throw new IllegalStateException("commit() must not be called in a dummy transaction");
-      }
-      tx.commit();
-      initDummy();
-    }
-
-    public void rollbackIfActive() {
-      if (isDummy()) {
-        throw new IllegalStateException("rollbackIfActive() must not be called in a dummy transaction");
-      }
-      if (tx.isActive()) {
-        tx.rollback();
-      }
-      initDummy();
-    }
-
-    public boolean isDummy() {
-      return tx == null;
-    }
-
-    public void initDummy() {
-      tx = null;
-      em.setFlushMode(FlushModeType.AUTO);
-    }
-  }
-
-  private ThreadLocal<CurrentTx> tx = new ThreadLocal<CurrentTx>();
+  private ThreadLocal<CurrentTx> txHolder = new ThreadLocal<CurrentTx>();
 
   private Provider<EntityManagerFactory> emf;
 
@@ -95,17 +23,20 @@ public class TxInterceptor implements MethodInterceptor {
   public <T> T invoke(Transactional ann, Callable<T> invocation) throws Exception {
     EntityManager em = null;
     boolean committed = false;
-    CurrentTx tx = this.tx.get();
+    CurrentTx tx = txHolder.get();
     try {
+
+      if (tx != null && !tx.isDummy()) {
+        // continue previously started 'real' transaction
+        tx.enter(ann);
+        return invocation.call();
+      }
+
       if (tx == null) {
         // init new 'dummy' or 'real' transaction
         em = emf.get().createEntityManager();
         tx = new CurrentTx(em, ann);
-        this.tx.set(tx);
-      } else if (!tx.isDummy()) {
-        // continue previously started 'real' transaction
-        tx.enter(ann);
-        return invocation.call();
+        txHolder.set(tx);
       }
 
       // continue or start 'dummy' transaction
@@ -132,7 +63,7 @@ public class TxInterceptor implements MethodInterceptor {
     } finally {
       // release entity manager and remove transaction if we have created it in this call
       if (em != null) {
-        this.tx.remove();
+        txHolder.remove();
         em.close();
       }
       if (committed)
@@ -142,31 +73,105 @@ public class TxInterceptor implements MethodInterceptor {
 
   @Override
   public Object invoke(final MethodInvocation invocation) throws Throwable {
-    return invoke(invocation.getMethod().getAnnotation(Transactional.class),
-            new Callable<Object>() {
-              @Override
-              public Object call() throws Exception {
-                try {
-                  return invocation.proceed();
-                } catch (Throwable throwable) {
-                  if (throwable instanceof Exception)
-                    throw (Exception)throwable;
-                  else
-                    throw new RuntimeException(throwable);
-                }
-              }
-            });
+    return invoke(
+        invocation.getMethod().getAnnotation(Transactional.class),
+        new Callable<Object>() {
+          @Override
+          public Object call() throws Exception {
+            try {
+              return invocation.proceed();
+            } catch (Throwable throwable) {
+              if (throwable instanceof Exception)
+                throw new Exception(throwable);
+              else
+                throw new RuntimeException(throwable);
+            }
+          }
+        });
   }
 
   public EntityManager currentEntityManager() {
-    CurrentTx tx = this.tx.get();
+    CurrentTx tx = txHolder.get();
     Preconditions.checkState(tx != null, "Not in transaction");
     return tx.em;
   }
 
   public PostCommitHooks currentPostCommitHooks() {
-    CurrentTx tx = this.tx.get();
+    CurrentTx tx = txHolder.get();
     Preconditions.checkState(tx != null, "Not in transaction");
     return tx.postCommitHooks;
+  }
+
+  private static class CurrentTx {
+    private final EntityManager em;
+    private boolean readOnly = false;
+    private EntityTransaction jpaTx = null; // null for dummy transactions
+    private final PostCommitHooks postCommitHooks = new PostCommitHooks();
+
+    public CurrentTx(EntityManager em, Transactional ann) {
+      this.em = em;
+      initTx(em, ann);
+    }
+
+    private void initTx(EntityManager em, Transactional ann) {
+      if (!ann.dummy()) {
+        readOnly = ann.readOnly();
+        jpaTx = em.getTransaction();
+        em.setFlushMode(readOnly ? FlushModeType.COMMIT : FlushModeType.AUTO);
+        if (ann.rollback()) {
+          jpaTx.setRollbackOnly();
+        }
+      } else {
+        initDummy();
+      }
+    }
+
+    public void enter(Transactional ann) {
+      if (isDummy()) {
+        initTx(em, ann);
+      } else {
+        if (!jpaTx.getRollbackOnly() && ann.rollback()) {
+          throw new IllegalStateException("Can't execute (rollback() == true) tx while in (rollback() == false) tx");
+        }
+        if (readOnly && !ann.readOnly()) {
+          em.setFlushMode(FlushModeType.AUTO);
+          readOnly = false;
+        }
+      }
+    }
+
+    public void begin() {
+      if (isDummy()) {
+        throw new IllegalStateException("begin() must not be called in a dummy transaction");
+      }
+      jpaTx.begin();
+    }
+
+    public void commit() {
+      if (isDummy()) {
+        throw new IllegalStateException("commit() must not be called in a dummy transaction");
+      }
+      jpaTx.commit();
+      initDummy();
+    }
+
+    public void rollbackIfActive() {
+      if (isDummy()) {
+        throw new IllegalStateException("rollbackIfActive() must not be called in a dummy transaction");
+      }
+      if (jpaTx.isActive()) {
+        jpaTx.rollback();
+      }
+      initDummy();
+    }
+
+    public boolean isDummy() {
+      return jpaTx == null;
+    }
+
+    public void initDummy() {
+      jpaTx = null;
+      em.setFlushMode(FlushModeType.AUTO);
+    }
   }
 }

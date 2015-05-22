1b7603aeac2974fc2605fc937ab9830aaa4b0019
Mikhail Fedotov <m.fedotov@hh.ru>
2015-05-22 13:12:59 +0300

    HH-52566 sync UriTool code to the one used and tested in hhid

--- nuts-and-bolts/src/main/java/ru/hh/util/UriTool.java
+++ nuts-and-bolts/src/main/java/ru/hh/util/UriTool.java
@@ -1,50 +1,27 @@
 package ru.hh.util;
 
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 public abstract class UriTool {
 
   private UriTool() {}
 
-  // handles badly encoded parameters or fragment parts properly
-  // (other methods often broke space characters in values, esp.
-  // space values in backurl parameters)
-  public static URI getUri(String url) throws URISyntaxException {
+  // handles badly encoded paths, parameters or fragment parts
+  public static URI getUri(String urlString) throws URISyntaxException {
     try {
-      return URI.create(url);
+      return URI.create(urlString);
     } catch (IllegalArgumentException e) {
-      // url parameters need to be escaped twice when passed as backurls,
-      // otherwise we will definitely get here from jersey calls.
-      int qPos = url.indexOf('?'); // queryPos (part after ?)
-      int fPos = url.indexOf('#'); // fragmentPos (part after #)
-      if (qPos == -1 && fPos == -1) {
-        throw e; // no reason to try anything if no query or fragment
+      // Url was provided without necessary escaping;
+      // pass through URL/URI constructors to apply proper escaping
+      try {
+        URL url = new URL(urlString);
+        return new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
+      } catch (MalformedURLException e1) {
+        throw new URISyntaxException(urlString, e1.getMessage());
       }
-      if (fPos > -1 && fPos < qPos) {
-        qPos = -1; // question mark is part of fragment, ignore it
-      }
-      String baseUrl = "";
-      if (qPos > 0) {
-        baseUrl = url.substring(0, qPos);
-      } else if (qPos == -1 && fPos > 0) {
-        baseUrl = url.substring(0, fPos);
-      }
-
-      String query = null;
-      if (qPos > -1) {
-        query = url.substring(qPos + 1, fPos > -1 ? fPos : url.length());
-      }
-
-      String fragment = null;
-      if (fPos > -1) {
-        fragment = url.substring(fPos + 1);
-      }
-
-      // Note: '?' and '#' are automatically added if needed
-      String fixedQueryAndFragment = new URI(null, null, null, query, fragment).toASCIIString();
-
-      return URI.create(baseUrl + fixedQueryAndFragment);
     }
   }
 }

ce340f0973bcd683eef8c17b7eb987faac174732
Mikhail Fedotov <m.fedotov@hh.ru>
2015-10-07 17:00:50 +0300

    HH-55638 add ability to configure extra pre-filters and post-filters for jersey

---
+++ nuts-and-bolts/src/main/java/ru/hh/nab/jersey/JerseyResourceFilterFactory.java
@@ -0,0 +1,50 @@
+package ru.hh.nab.jersey;
+
+import com.google.common.collect.Lists;
+import com.sun.jersey.api.model.AbstractMethod;
+import com.sun.jersey.spi.container.ResourceFilter;
+import com.sun.jersey.spi.container.ResourceFilterFactory;
+import java.util.List;
+import ru.hh.nab.security.PermissionLoader;
+import ru.hh.nab.security.SecurityFilter;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Singleton
+public class JerseyResourceFilterFactory implements ResourceFilterFactory {
+  private final PermissionLoader permissionLoader;
+  private final List<ResourceFilterFactory> preFilterFactories;
+  private final List<ResourceFilterFactory> postFilterFactories;
+
+  @Inject
+  public JerseyResourceFilterFactory(
+    PermissionLoader permissionLoader,
+    @Named("JerseyPreFilterFactories") List<ResourceFilterFactory> preFilterFactories,
+    @Named("JerseyPostFilterFactories") List<ResourceFilterFactory> postFilterFactories
+  ) {
+    this.permissionLoader = permissionLoader;
+    this.preFilterFactories = preFilterFactories;
+    this.postFilterFactories = postFilterFactories;
+  }
+
+  @Override
+  public List<ResourceFilter> create(AbstractMethod am) {
+    List<ResourceFilter> filters = Lists.newArrayList();
+    preFilterFactories.forEach(ff -> filters.addAll(ff.create(am)));
+    addCommonFilters(filters, am);
+    postFilterFactories.forEach(ff -> filters.addAll(ff.create(am)));
+    return filters;
+  }
+
+  private void addCommonFilters(List<ResourceFilter> filters, AbstractMethod am) {
+    filters.add(new FreemarkerModelFilter());
+    if (am.isAnnotationPresent(Cached.class)) {
+      filters.add(new CacheControlFilter(am.getAnnotation(Cached.class)));
+    }
+    filters.add(new NginxQuirksFilter());
+    filters.add(new SecurityFilter(permissionLoader));
+  }
+}

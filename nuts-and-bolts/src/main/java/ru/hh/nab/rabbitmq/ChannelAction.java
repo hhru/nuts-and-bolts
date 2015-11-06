f71d52d894bbc8dddc72d80357b4a6134cda04ec
Mikhail Fedotov <m.fedotov@hh.ru>
2015-11-06 15:58:45 +0300

    HH-42642 remove old unused rabbit client code

--- nuts-and-bolts/src/main/java/ru/hh/nab/rabbitmq/ChannelAction.java
+++
@@ -1,9 +0,0 @@
-package ru.hh.nab.rabbitmq;
-
-import com.rabbitmq.client.Channel;
-
-import java.io.IOException;
-
-public interface ChannelAction {
-  void perform(Channel channel) throws IOException;
-}

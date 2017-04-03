b6a1a5c8c51e37f2a4dc8e03fad0844a0c6f7384
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2017-04-03 14:09:08 +0300

    HH-67418 refactores test to init properties

--- nuts-and-bolts/src/test/java/ru/hh/nab/SettingsModuleTest.java
+++ nuts-and-bolts/src/test/java/ru/hh/nab/SettingsModuleTest.java
@@ -1,11 +1,12 @@
 package ru.hh.nab;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Properties;
 import org.junit.After;
@@ -16,32 +17,43 @@ public class SettingsModuleTest {
 
   @Test
   public void testLoadProperties() throws IOException {
-    File settingsDir = prepareSettingsDir("port=1111", null);
+    Properties defaultSettings = new Properties() {{
+      put("port", "1111");
+    }};
+    File settingsDir = prepareSettingsDir(defaultSettings, null);
     Properties props = SettingsModule.loadProperties(settingsDir);
     assertEquals("1111", props.getProperty("port"));
   }
 
   @Test
   public void testLoadPropertiesOverriden() throws IOException {
-    File settingsDir = prepareSettingsDir("port=1111", "port=2222");
+    Properties defaultSettings = new Properties() {{
+      put("port", "1111");
+    }};
+    Properties overrideSettings = new Properties() {{
+      put("port", "2222");
+    }};
+    File settingsDir = prepareSettingsDir(defaultSettings, overrideSettings);
     Properties props = SettingsModule.loadProperties(settingsDir);
     assertEquals("2222", props.getProperty("port"));
   }
 
-  private static File prepareSettingsDir(String defaultValue, String overrideValue) throws IOException {
+  private static File prepareSettingsDir(Properties defaultSettings, Properties overrideSettings) throws IOException {
     File directory = Files.createTempDirectory(SettingsModuleTest.class.getName()).toFile();
     directory.mkdirs();
-    if (defaultValue != null) {
-      File defaultProperties = new File(directory, "settings.properties");
-      defaultProperties.createNewFile();
-      Files.write(defaultProperties.toPath(), Collections.singleton(defaultValue));
-    }
-    if (overrideValue != null) {
-      File properties = new File(directory, "settings.properties.dev");
-      properties.createNewFile();
-      Files.write(properties.toPath(), Collections.singleton(overrideValue));
-    }
+    writeProperties(directory, defaultSettings, "settings.properties");
+    writeProperties(directory, overrideSettings, "settings.properties.dev");
     return directory;
+  }
+
+  private static void writeProperties(File directory, Properties settings, String fileName) throws IOException {
+    if (settings != null) {
+      File targetFile = new File(directory, fileName);
+      targetFile.createNewFile();
+      try (Writer writer = new FileWriter(targetFile)) {
+        settings.store(writer, "");
+      }
+    }
   }
 
   @After

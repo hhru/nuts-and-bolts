cbeaf2cd85f505926fc3a7e623899981d3882027
Artur Khalikov <a.khalikov@hh.ru>
2018-07-03 17:06:52 +0300

    HH-77163 support packagesToScan for hibernate session factory

--- nab-hibernate/src/test/java/ru/hh/nab/hibernate/HibernateTestBaseConfig.java
+++ nab-hibernate/src/test/java/ru/hh/nab/hibernate/HibernateTestBaseConfig.java
@@ -3,6 +3,7 @@ package ru.hh.nab.hibernate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import ru.hh.nab.hibernate.model.TestEntity;
 
 @Configuration
 @Import({
@@ -10,10 +11,12 @@ import org.springframework.context.annotation.Import;
   HibernateCommonConfig.class
 })
 public class HibernateTestBaseConfig {
+  static final String TEST_PACKAGE = "ru.hh.nab.hibernate.model.test";
+
   @Bean
   MappingConfig mappingConfig() {
-    MappingConfig mappingConfig = new MappingConfig();
-    mappingConfig.addMapping(TestEntity.class);
+    MappingConfig mappingConfig = new MappingConfig(TestEntity.class);
+    mappingConfig.addPackagesToScan(TEST_PACKAGE);
     return mappingConfig;
   }
 }

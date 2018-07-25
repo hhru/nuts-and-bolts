2b881261e84faa5f33a5e107df4594a63d4f4f65
Smirnov Danil <d.smirnov@hh.ru>
2018-07-25 17:19:20 +0300

    HH-81389 Run shutdown hook on application exit

--- nab-starter/src/main/java/ru/hh/nab/starter/NabApplication.java
+++ nab-starter/src/main/java/ru/hh/nab/starter/NabApplication.java
@@ -43,6 +43,7 @@ public class NabApplication {
     final NabApplicationContext context = new NabApplicationContext();
     context.register(primarySources);
     context.refresh();
+    context.registerShutdownHook();
     return context;
   }
 

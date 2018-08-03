32b419783f27dafe47401dd42637ab3aa842461c
Artur Khalikov <akhalikoff@gmail.com>
2018-08-03 16:31:12 +0300

    HH-80325 update nab-example

--- nab-example/src/main/java/ru/hh/nab/example/ExampleMain.java
+++ nab-example/src/main/java/ru/hh/nab/example/ExampleMain.java
@@ -1,14 +1,22 @@
 package ru.hh.nab.example;
 
+import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import ru.hh.nab.starter.NabApplication;
+import ru.hh.nab.starter.NabProdConfig;
+import ru.hh.nab.starter.servlet.DefaultServletConfig;
 
 @Configuration
-@Import(ExampleProdConfig.class)
+@Import({NabProdConfig.class})
 public class ExampleMain {
 
   public static void main(String[] args) {
-    NabApplication.run(ExampleMain.class);
+    NabApplication.run(new DefaultServletConfig() {
+      @Override
+      public void registerResources(ResourceConfig resourceConfig) {
+        resourceConfig.register(ExampleResource.class);
+      }
+    }, ExampleMain.class);
   }
 }

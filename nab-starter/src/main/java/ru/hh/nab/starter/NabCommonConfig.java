8e5e050bfa85d59c021f0580632bab04efa56339
Artur Khalikov <akhalikoff@gmail.com>
2018-08-16 00:27:44 +0300

    HH-81279 improve datacenter support

--- nab-starter/src/main/java/ru/hh/nab/starter/NabCommonConfig.java
+++ nab-starter/src/main/java/ru/hh/nab/starter/NabCommonConfig.java
@@ -1,6 +1,7 @@
 package ru.hh.nab.starter;
 
 import static java.util.Optional.ofNullable;
+import java.util.concurrent.ScheduledExecutorService;
 import org.eclipse.jetty.util.thread.ThreadPool;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,23 +9,14 @@ import ru.hh.nab.common.executor.ScheduledExecutor;
 import ru.hh.nab.common.properties.FileSettings;
 import static ru.hh.nab.starter.server.jetty.JettyServerFactory.createJettyThreadPool;
 
-import java.util.concurrent.ScheduledExecutorService;
-
 @Configuration
 public class NabCommonConfig {
   private static final String SERVICE_NAME_PROPERTY = "serviceName";
-  private static final String DATACENTER_NAME_PROPERTY = "datacenter";
 
   @Bean
   String serviceName(FileSettings fileSettings) {
     return ofNullable(fileSettings.getString(SERVICE_NAME_PROPERTY))
         .orElseThrow(() -> new RuntimeException(String.format("'%s' property is not found in file settings", SERVICE_NAME_PROPERTY)));
-  }
-
-  @Bean
-  String datacenter(FileSettings fileSettings) {
-    return ofNullable(fileSettings.getString(DATACENTER_NAME_PROPERTY))
-      .orElseThrow(() -> new RuntimeException(String.format("'%s' property is not found in file settings", DATACENTER_NAME_PROPERTY)));
   }
 
   @Bean

f4a0934884f98e3baa860257e2343955ea52bb75
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2018-11-02 21:37:51 +0300

    HH-81790 add test for status

--- nab-tests/src/test/java/ru/hh/nab/starter/NabApplicationTest.java
+++ nab-tests/src/test/java/ru/hh/nab/starter/NabApplicationTest.java
@@ -3,7 +3,15 @@ package ru.hh.nab.starter;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.springframework.web.context.support.WebApplicationContextUtils.getWebApplicationContext;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import org.junit.Test;
+import org.springframework.web.context.WebApplicationContext;
 import ru.hh.nab.starter.server.jetty.JettyServer;
 import ru.hh.nab.testbase.NabTestConfig;
 
@@ -12,8 +20,29 @@ public class NabApplicationTest {
   @Test
   public void runShouldStartJetty() {
     JettyServer server = NabApplication.runDefaultWebApp(NabTestConfig.class);
+    WebApplicationContext webApplicationContext = getWebApplicationContext(server.getServletContext());
+    AppMetadata appMetadata = webApplicationContext.getBean(AppMetadata.class);
+    long upTimeSeconds = appMetadata.getUpTimeSeconds();
+    assertEquals(NabTestConfig.TEST_SERVICE_NAME, webApplicationContext.getBean("serviceName"));
+    Invocation.Builder statusReq = ClientBuilder.newBuilder().build().target(UriBuilder.fromUri("http://localhost").port(server.getPort()).build())
+      .path("status").request();
+    try (Response response = statusReq.get()) {
+      assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+      Project project = response.readEntity(Project.class);
+      assertEquals(appMetadata.getServiceName(), project.name);
+      assertEquals(appMetadata.getVersion(), project.version);
+      assertTrue(project.uptime >= upTimeSeconds);
+    }
 
-    assertTrue(server.isRunning());
-    assertEquals(NabTestConfig.TEST_SERVICE_NAME, getWebApplicationContext(server.getServletContext()).getBean("serviceName"));
+  }
+
+  @XmlRootElement
+  private static final class Project {
+    @XmlAttribute
+    private String name;
+    @XmlElement
+    private String version;
+    @XmlElement
+    private long uptime;
   }
 }

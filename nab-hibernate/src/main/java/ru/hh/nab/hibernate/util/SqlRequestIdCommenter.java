cdf866f094146a2d8ba26c36979e477afb59b214
Andrew Sumin <sumin.andrew@gmail.com>
2018-12-18 14:41:04 +0300

    HH-86450 allow more chars in SqlRequestIdCommenter

--- nab-hibernate/src/main/java/ru/hh/nab/hibernate/util/SqlRequestIdCommenter.java
+++ nab-hibernate/src/main/java/ru/hh/nab/hibernate/util/SqlRequestIdCommenter.java
@@ -1,12 +1,16 @@
 package ru.hh.nab.hibernate.util;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.hh.nab.common.mdc.MDC;
 
-public final class SqlRequestIdCommenter {
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
+import static java.util.regex.Pattern.CASE_INSENSITIVE;
+
+public final class SqlRequestIdCommenter {
+  private static final Predicate<String> SQL_REQUEST_ID_IS_VALID = Pattern.compile("^[a-z0-9_-]+$", CASE_INSENSITIVE).asPredicate();
   private static final Logger LOGGER = LoggerFactory.getLogger(SqlRequestIdCommenter.class);
 
   private SqlRequestIdCommenter() {}
@@ -14,7 +18,7 @@ public final class SqlRequestIdCommenter {
   public static String addRequestIdComment(final String sql) {
     return MDC.getRequestId().map(requestId -> {
       // check for sql injections and reasonable length
-      if (requestId.length() > 100 || !StringUtils.isAlphanumeric(requestId)) {
+      if (requestId.length() > 100 || !SQL_REQUEST_ID_IS_VALID.test(requestId)) {
         LOGGER.warn("Errant request id, not including it to SQL query: {}", requestId);
         return sql;
       }

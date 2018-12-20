934fbb368873d4aac6e44474b8ccb0e5a874a2d4
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2018-12-20 17:23:32 +0300

    HH-86630 fallback layout

--- nab-common/src/main/java/ru/hh/nab/common/logging/HhSyslogAppender.java
+++ nab-common/src/main/java/ru/hh/nab/common/logging/HhSyslogAppender.java
@@ -1,6 +1,8 @@
 package ru.hh.nab.common.logging;
 
+import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.core.Context;
+import ch.qos.logback.core.Layout;
 import com.papertrailapp.logback.Syslog4jAppender;
 import org.productivity.java.syslog4j.SyslogConstants;
 import org.productivity.java.syslog4j.impl.net.udp.UDPNetSyslogConfig;
@@ -9,6 +11,15 @@ public class HhSyslogAppender extends Syslog4jAppender {
 
   @Override
   public void start() {
+    Layout<?> layout = getLayout();
+    if (layout == null) {
+      PatternLayout patternLayout = new PatternLayout();
+      patternLayout.setContext(context);
+      patternLayout.setPattern(context.getProperty("log.pattern"));
+      setLayout(patternLayout);
+      getLayout().start();
+
+    }
     Context context = getContext();
     String host = context.getProperty("log.syslogHost");
     String port = context.getProperty("log.syslogPort");

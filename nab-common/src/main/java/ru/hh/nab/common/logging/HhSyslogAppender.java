b737d6325b2d96a5e571a28146ded34b19f07a96
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2018-12-24 19:09:38 +0300

    HH-86864 increase message length

--- nab-common/src/main/java/ru/hh/nab/common/logging/HhSyslogAppender.java
+++ nab-common/src/main/java/ru/hh/nab/common/logging/HhSyslogAppender.java
@@ -3,10 +3,13 @@ package ru.hh.nab.common.logging;
 import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.core.Layout;
 import com.papertrailapp.logback.Syslog4jAppender;
+import java.util.Optional;
 import org.productivity.java.syslog4j.SyslogConstants;
 import org.productivity.java.syslog4j.impl.net.udp.UDPNetSyslogConfig;
 
 public class HhSyslogAppender extends Syslog4jAppender {
+
+  private static final int DEFAULT_MSG_LENGTH = 60000;
 
   @Override
   public void start() {
@@ -19,6 +22,16 @@ public class HhSyslogAppender extends Syslog4jAppender {
     String port = context.getProperty("log.syslogPort");
     UDPNetSyslogConfig udpNetSyslogConfig = new UDPNetSyslogConfig(SyslogConstants.FACILITY_USER, host, Integer.valueOf(port));
     udpNetSyslogConfig.setIdent(getName());
+    udpNetSyslogConfig.setSendLocalName(false);
+    udpNetSyslogConfig.setSendLocalTimestamp(false);
+    int maxMessageLength = Optional.ofNullable(context.getProperty("log.syslogMaxMessageLength")).map(length -> {
+      try {
+        return Integer.valueOf(length);
+      } catch (NumberFormatException e) {
+        return null;
+      }
+    }).orElse(DEFAULT_MSG_LENGTH);
+    udpNetSyslogConfig.setMaxMessageLength(maxMessageLength);
     setSyslogConfig(udpNetSyslogConfig);
     super.start();
   }

c764348a8e19bff4ebffe859f7486ee276750925
Irina Garanina <i.garanina@hh.ru>
2019-01-24 16:00:23 +0300

    HH-87714 jetty 9.4.13 -> jetty 9.4.14

---
+++ nab-tests/src/test/java/ru/hh/nab/starter/requestscope/RequestDetails.java
@@ -0,0 +1,9 @@
+package ru.hh.nab.starter.requestscope;
+
+public class RequestDetails {
+  private final String field = "qwerty";
+
+  public String getField() {
+    return field;
+  }
+}
---
+++ nab-tests/src/test/java/ru/hh/nab/starter/requestscope/RequestScopeTest.java
@@ -0,0 +1,42 @@
+package ru.hh.nab.starter.requestscope;
+
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import ru.hh.nab.starter.NabApplication;
+import ru.hh.nab.testbase.NabTestBase;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.Response.Status.OK;
+import static org.junit.Assert.assertEquals;
+
+@ContextConfiguration(classes = {RequestConfig.class})
+public class RequestScopeTest extends NabTestBase {
+
+  @Inject
+  private Provider<RequestDetails> requestProvider;
+
+  @Test
+  public void requestScopeTest() {
+
+    final String name = requestProvider.get().getField();
+    Response response = target("/hello")
+        .queryParam("name", name)
+        .request()
+        .get();
+    assertEquals(OK.getStatusCode(), response.getStatus());
+    assertEquals(String.format("Hello, %s!", name), response.readEntity(String.class));
+  }
+
+  @Override
+  protected NabApplication getApplication() {
+    return NabApplication
+        .builder()
+        .configureJersey()
+        .registerResources(TestResource.class)
+        .bindToRoot()
+        .build();
+  }
+}

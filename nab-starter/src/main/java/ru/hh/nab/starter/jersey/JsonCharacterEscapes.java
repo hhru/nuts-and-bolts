839b2b2108d7c4512b4f47e4c16f057569fc778c
Andrew Sumin <sumin.andrew@gmail.com>
2019-02-08 17:38:46 +0300

    HH-88253 add jackson escaping, improve jaxb escaping

---
+++ nab-starter/src/main/java/ru/hh/nab/starter/jersey/JsonCharacterEscapes.java
@@ -0,0 +1,33 @@
+package ru.hh.nab.starter.jersey;
+
+import com.fasterxml.jackson.core.JsonpCharacterEscapes;
+import com.fasterxml.jackson.core.SerializableString;
+import com.fasterxml.jackson.core.io.CharacterEscapes;
+import com.fasterxml.jackson.core.io.SerializedString;
+
+import java.util.stream.IntStream;
+
+public class JsonCharacterEscapes extends JsonpCharacterEscapes {
+  private static final SerializableString REPLACEMENT_STR = new SerializedString(String.valueOf(CharacterEscapeBase.REPLACEMENT_CHAR));
+
+  private static final int[] asciiEscapes = CharacterEscapes.standardAsciiEscapesForJSON();
+  static {
+    IntStream.range(0, 0x20)
+      .filter(i -> CharacterEscapeBase.isInvalidTextSymbol((char) i))
+      .forEach(i -> asciiEscapes[i] = CharacterEscapes.ESCAPE_CUSTOM);
+  }
+
+  @Override
+  public int[] getEscapeCodesForAscii() {
+    return asciiEscapes;
+  }
+
+  @Override
+  public SerializableString getEscapeSequence(int i) {
+    if (CharacterEscapeBase.isInvalidTextSymbol((char) i)) {
+      return REPLACEMENT_STR;
+    }
+
+    return super.getEscapeSequence(i);
+  }
+}
---
+++ nab-starter/src/main/java/ru/hh/nab/starter/jersey/ObjectMapperContextResolver.java
@@ -0,0 +1,17 @@
+package ru.hh.nab.starter.jersey;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.ws.rs.ext.ContextResolver;
+
+public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
+  public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  static {
+    OBJECT_MAPPER.getFactory().setCharacterEscapes(new JsonCharacterEscapes());
+  }
+
+  @Override
+  public ObjectMapper getContext(Class<?> type) {
+    return OBJECT_MAPPER;
+  }
+}

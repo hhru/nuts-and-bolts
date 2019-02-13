808d62089aeaceee781c3a094058cab60632642c
y.marinov <y.marinov@hh.ru>
2019-02-13 17:13:52 +0300

    HH-88551 add ability to set HikariConfig in DataSourceFactory

--- nab-data-source/src/main/java/ru/hh/nab/datasource/DataSourceFactory.java
+++ nab-data-source/src/main/java/ru/hh/nab/datasource/DataSourceFactory.java
@@ -1,7 +1,14 @@
 package ru.hh.nab.datasource;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import static java.lang.Integer.parseInt;
+import java.sql.Connection;
+import java.sql.SQLException;
 import static java.util.Optional.ofNullable;
+import java.util.Properties;
+import javax.sql.DataSource;
+import ru.hh.nab.common.properties.FileSettings;
 import static ru.hh.nab.datasource.DataSourceSettings.DEFAULT_VALIDATION_TIMEOUT_INCREMENT_MS;
 import static ru.hh.nab.datasource.DataSourceSettings.JDBC_URL;
 import static ru.hh.nab.datasource.DataSourceSettings.MONITORING_SEND_STATS;
@@ -9,17 +16,8 @@ import static ru.hh.nab.datasource.DataSourceSettings.PASSWORD;
 import static ru.hh.nab.datasource.DataSourceSettings.POOL_SETTINGS_PREFIX;
 import static ru.hh.nab.datasource.DataSourceSettings.STATEMENT_TIMEOUT_MS;
 import static ru.hh.nab.datasource.DataSourceSettings.USER;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import ru.hh.nab.common.properties.FileSettings;
 import ru.hh.nab.datasource.monitoring.AbstractMetricsTrackerFactoryProvider;
 import ru.hh.nab.datasource.monitoring.StatementTimeoutDataSource;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Properties;
 
 public class DataSourceFactory {
   private final AbstractMetricsTrackerFactoryProvider metricsTrackerFactoryProvider;
@@ -32,8 +30,37 @@ public class DataSourceFactory {
     return createDataSource(dataSourceName, isReadonly, settings.getSubSettings(dataSourceName));
   }
 
+  public DataSource create(HikariConfig hikariConfig, FileSettings monitoringSettings) {
+    boolean sendStats = ofNullable(monitoringSettings.getBoolean(MONITORING_SEND_STATS)).orElse(false);
+    if (sendStats && metricsTrackerFactoryProvider != null) {
+      hikariConfig.setMetricsTrackerFactory(metricsTrackerFactoryProvider.create(monitoringSettings));
+    }
+    return new HikariDataSource(hikariConfig);
+  }
+
+  public static HikariConfig createBasesHikariConfig(String dataSourceName, boolean isReadonly, FileSettings dataSourceSettings) {
+    Properties poolProperties = dataSourceSettings.getSubProperties(POOL_SETTINGS_PREFIX);
+    if (poolProperties.isEmpty()) {
+      throw new RuntimeException(String.format(
+          "Exception during %1$s pooled datasource initialization: could not find %1$s.%2$s settings in config file. " +
+              "To prevent misconfiguration application startup will be aborted.",
+          dataSourceName, POOL_SETTINGS_PREFIX
+      ));
+    }
+
+    HikariConfig config = new HikariConfig(poolProperties);
+    config.setJdbcUrl(dataSourceSettings.getString(JDBC_URL));
+    config.setUsername(dataSourceSettings.getString(USER));
+    config.setPassword(dataSourceSettings.getString(PASSWORD));
+    config.setPoolName(dataSourceName);
+    config.setReadOnly(isReadonly);
+    config.setValidationTimeout(config.getConnectionTimeout() + DEFAULT_VALIDATION_TIMEOUT_INCREMENT_MS);
+    return config;
+  }
+
   protected DataSource createDataSource(String dataSourceName, boolean isReadonly, FileSettings dataSourceSettings) {
-    DataSource underlyingDataSource = createPooledDatasource(dataSourceName, isReadonly, dataSourceSettings);
+    HikariConfig hikariConfig = createBasesHikariConfig(dataSourceName, isReadonly, dataSourceSettings);
+    DataSource underlyingDataSource = create(hikariConfig, dataSourceSettings);
 
     String statementTimeoutMsVal = dataSourceSettings.getString(STATEMENT_TIMEOUT_MS);
     if (statementTimeoutMsVal != null) {
@@ -46,32 +73,6 @@ public class DataSourceFactory {
     checkDataSource(underlyingDataSource, dataSourceName);
 
     return underlyingDataSource;
-  }
-
-  private DataSource createPooledDatasource(String dataSourceName, boolean isReadonly, FileSettings dataSourceSettings) {
-    Properties poolProperties = dataSourceSettings.getSubProperties(POOL_SETTINGS_PREFIX);
-    if (poolProperties.isEmpty()) {
-      throw new RuntimeException(String.format(
-        "Exception during %1$s pooled datasource initialization: could not find %1$s.%2$s settings in config file. " +
-        "To prevent misconfiguration application startup will be aborted.",
-        dataSourceName, POOL_SETTINGS_PREFIX
-      ));
-    }
-
-    HikariConfig config = new HikariConfig(poolProperties);
-    config.setJdbcUrl(dataSourceSettings.getString(JDBC_URL));
-    config.setUsername(dataSourceSettings.getString(USER));
-    config.setPassword(dataSourceSettings.getString(PASSWORD));
-    config.setPoolName(dataSourceName);
-    config.setReadOnly(isReadonly);
-    config.setValidationTimeout(config.getConnectionTimeout() + DEFAULT_VALIDATION_TIMEOUT_INCREMENT_MS);
-
-    boolean sendStats = ofNullable(dataSourceSettings.getBoolean(MONITORING_SEND_STATS)).orElse(false);
-    if (sendStats && metricsTrackerFactoryProvider != null) {
-      config.setMetricsTrackerFactory(metricsTrackerFactoryProvider.create(dataSourceSettings));
-    }
-
-    return new HikariDataSource(config);
   }
 
   private static void checkDataSource(DataSource dataSource, String dataSourceName) {

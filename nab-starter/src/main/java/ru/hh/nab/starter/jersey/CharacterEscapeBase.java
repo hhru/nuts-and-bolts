47049d87114092e9b879ef72683909aa15420072
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2019-02-28 15:39:56 +0300

    publish class to make usable

--- nab-starter/src/main/java/ru/hh/nab/starter/jersey/CharacterEscapeBase.java
+++ nab-starter/src/main/java/ru/hh/nab/starter/jersey/CharacterEscapeBase.java
@@ -1,11 +1,11 @@
 package ru.hh.nab.starter.jersey;
 
-final class CharacterEscapeBase {
+public final class CharacterEscapeBase {
   private CharacterEscapeBase() {}
 
   static final char REPLACEMENT_CHAR = '\uFFFD';
 
-  static boolean isInvalidTextSymbol(char c) {
+  public static boolean isInvalidTextSymbol(char c) {
     return (c < 0x20 && c != 0x9 && c != 0xA && c != 0xD) || (c == 0xFFFE || c == 0xFFFF);
   }
 }

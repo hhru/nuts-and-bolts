fef89f9f13f17f9941d9eceb5337cc40d6bfae74
Vladislav Senin <troy4eg@gmail.com>
2019-02-28 12:15:07 +0300

    HH-89308 fixed syslog bug

--- nab-logging/src/test/java/ru/hh/nab/logging/HhSyslogAppenderTest.java
+++ nab-logging/src/test/java/ru/hh/nab/logging/HhSyslogAppenderTest.java
@@ -4,10 +4,14 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.AppenderBase;
 import ch.qos.logback.core.Context;
+import org.junit.Test;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.nio.charset.Charset;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
@@ -15,7 +19,7 @@ import java.util.Collections;
 import java.util.concurrent.Exchanger;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
-import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class HhSyslogAppenderTest {
@@ -29,8 +33,22 @@ public class HhSyslogAppenderTest {
     };
     LocalDateTime epochStart = LocalDateTime.ofEpochSecond(0, 0, OffsetDateTime.now().getOffset());
     testLogging(hhSyslogAppenderFunction, "test", "<11>test: ["
+        + epochStart.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS"))
+        + "] ERROR logger:1 mdc={} - message",
+      "message");
+  }
+
+  @Test
+  public void testLoadPatternWithCyrillicSymbols() throws Exception {
+    Function<Context, HhSyslogAppender> hhSyslogAppenderFunction = context -> {
+      HhSyslogAppender appender = new HhSyslogAppender();
+      appender.setContext(context);
+      return appender;
+    };
+    LocalDateTime epochStart = LocalDateTime.ofEpochSecond(0, 0, OffsetDateTime.now().getOffset());
+    testLogging(hhSyslogAppenderFunction, "test", "<11>test: ["
       + epochStart.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS"))
-      + "] ERROR logger:1 mdc={} - message");
+      + "] ERROR logger:1 mdc={} - сообщение", "сообщение");
   }
 
   @Test
@@ -45,41 +63,44 @@ public class HhSyslogAppenderTest {
       patternLayout.start();
       return appender;
     };
-    testLogging(hhSyslogAppenderFunction, "test", "<11>test: message");
+    testLogging(hhSyslogAppenderFunction, "test", "<11>test: message", "message");
   }
 
-  protected void testLogging(Function<Context, HhSyslogAppender> appenderCreateFunction, String pid, String expected) throws Exception {
+  protected void testLogging(Function<Context, ? extends AppenderBase> appenderCreateFunction, String pid, String expected,
+                             String message) throws Exception {
     DatagramSocket serverSocket = new DatagramSocket();
     LoggerContext context = new LoggerContext();
     context.putProperty("log.syslogHost", "localhost");
     context.putProperty("log.syslogPort", String.valueOf(serverSocket.getLocalPort()));
     context.putProperty("log.pattern", "[%date{ISO8601}] %-5level %logger{36}:%line mdc={%mdc} - %msg%n");
-    HhSyslogAppender appender = appenderCreateFunction.apply(context);
+    var appender = appenderCreateFunction.apply(context);
     appender.setName(pid);
     appender.start();
     Exchanger<String> exchanger = new Exchanger<>();
     Thread checker = new Thread(() -> {
-      byte[] receiveData = new byte[1024];
+      byte[] receiveData = new byte[4096];
       try {
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
         serverSocket.receive(receivePacket);
-        String message = new String(receivePacket.getData()).trim();
-        exchanger.exchange(message);
+        String msg = new String(receivePacket.getData(), Charset.forName("UTF-8")).trim();
+        exchanger.exchange(msg);
       } catch (IOException | InterruptedException e) {
         throw new RuntimeException(e);
       }
     });
     checker.start();
     LoggingEvent eventObject = new LoggingEvent();
-    eventObject.setCallerData(new StackTraceElement[] {new StackTraceElement("class", "method", null, 1)});
+    eventObject.setCallerData(new StackTraceElement[]{
+      new StackTraceElement("class", "method", null, 1)
+    });
     eventObject.setMDCPropertyMap(Collections.emptyMap());
     eventObject.setLevel(Level.ERROR);
     eventObject.setTimeStamp(0);
-    eventObject.setMessage("message");
+    eventObject.setMessage(message);
     eventObject.setLoggerName("logger");
     appender.doAppend(eventObject);
-    String message = exchanger.exchange(null, 5, TimeUnit.SECONDS);
-    assertEquals(expected, message);
+    String actual = exchanger.exchange(null, 5, TimeUnit.SECONDS);
+    assertEquals(expected, actual);
   }
 
 }

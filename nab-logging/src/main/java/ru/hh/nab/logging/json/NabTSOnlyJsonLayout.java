f45b55562247103bdbdfaa59ec17c14d63c9e1e8
Andrew Sumin <sumin.andrew@gmail.com>
2019-03-06 15:26:18 +0300

    HH-89435 fix ts_only json layout

--- nab-logging/src/main/java/ru/hh/nab/logging/json/NabTSOnlyJsonLayout.java
+++ nab-logging/src/main/java/ru/hh/nab/logging/json/NabTSOnlyJsonLayout.java
@@ -5,11 +5,10 @@ import net.logstash.logback.layout.LogstashLayout;
 import static ru.hh.nab.logging.json.JsonFieldNames.DEFAULT_TIMESTAMP_FORMAT;
 
 public class NabTSOnlyJsonLayout extends LogstashLayout {
-
   public NabTSOnlyJsonLayout() {
     super();
 
-    setFieldNames(LogstashFields.DEFAULT.getFieldNames());
+    setFieldNames(LogstashFields.TS_ONLY.getFieldNames());
     setIncludeMdc(false);
     setIncludeContext(false);
     setIncludeCallerData(false);

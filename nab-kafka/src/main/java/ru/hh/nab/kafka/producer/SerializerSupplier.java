305c7df23b5486b0d6e3487b9f714331b8f7398b
Pavel Orlov <p.orlov@hh.ru>
2020-06-24 17:36:58 +0300

    HH-111733 add @FunctionalInterface for SerializerSupplier

--- nab-kafka/src/main/java/ru/hh/nab/kafka/producer/SerializerSupplier.java
+++ nab-kafka/src/main/java/ru/hh/nab/kafka/producer/SerializerSupplier.java
@@ -2,6 +2,7 @@ package ru.hh.nab.kafka.producer;
 
 import org.apache.kafka.common.serialization.Serializer;
 
+@FunctionalInterface
 public interface SerializerSupplier {
 
   <T> Serializer<T> supply();

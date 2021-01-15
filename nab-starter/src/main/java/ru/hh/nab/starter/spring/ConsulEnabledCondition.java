ef5a86f8aed67c12b3f6de233fbe65f1bf2a1f5f
d.zharikhin <d.zharikhin@hh.ru>
2021-01-15 19:36:18 +0300

    HH-122108 fix potential NPE

--- nab-starter/src/main/java/ru/hh/nab/starter/spring/ConsulEnabledCondition.java
+++ nab-starter/src/main/java/ru/hh/nab/starter/spring/ConsulEnabledCondition.java
@@ -1,5 +1,6 @@
 package ru.hh.nab.starter.spring;
 
+import static java.util.Optional.ofNullable;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
@@ -10,7 +11,7 @@ public final class ConsulEnabledCondition implements Condition {
 
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        FileSettings fileSettings = context.getBeanFactory().getBean(FileSettings.class);
-        return fileSettings.getBoolean(ConsulService.CONSUL_ENABLED_PROPERTY, true);
+        return ofNullable(context.getBeanFactory()).map(factory -> factory.getBean(FileSettings.class))
+          .map(fileSettings -> fileSettings.getBoolean(ConsulService.CONSUL_ENABLED_PROPERTY, true)).orElse(Boolean.FALSE);
     }
 }

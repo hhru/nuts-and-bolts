01f6f756c6a885aa97a338b623bb444a26eeb961
d.zharikhin <d.zharikhin@hh.ru>
2021-07-27 13:39:04 +0300

    HH-133623 add ability to apply config overrides

--- nab-common/src/main/java/ru/hh/nab/common/properties/PropertiesUtils.java
+++ nab-common/src/main/java/ru/hh/nab/common/properties/PropertiesUtils.java
@@ -2,13 +2,17 @@ package ru.hh.nab.common.properties;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class PropertiesUtils {
   public static final String SETINGS_DIR_PROPERTY = "settingsDir";
+  static final String OVERRIDE_FOLDER_POSTFIX = ".d";
   static final String DEFAULT_DEV_FILE_EXT = ".dev";
 
   public static Properties fromFilesInSettingsDir(String fileName) throws IOException {
@@ -16,15 +20,18 @@ public class PropertiesUtils {
   }
 
   public static Properties fromFilesInSettingsDir(String fileName, String devFileName) throws IOException {
-    final String settingsDir = System.getProperty(SETINGS_DIR_PROPERTY, ".");
-    final Properties properties = new Properties();
+    Path filePath = Paths.get(System.getProperty(SETINGS_DIR_PROPERTY, ".")).resolve(fileName);
+    Properties properties = new Properties();
 
-    final Path defaultPath = Paths.get(settingsDir, fileName);
-    try (InputStream inputStream = Files.newInputStream(defaultPath)) {
-      properties.load(inputStream);
-    }
+    loadFromFileWithOverrides(filePath, configPath -> {
+      try (InputStream inputStream = Files.newInputStream(configPath)) {
+        properties.load(inputStream);
+      } catch (IOException e) {
+        throw new UncheckedIOException(e);
+      }
+    });
 
-    final Path customPath = Paths.get(settingsDir, devFileName);
+    Path customPath = filePath.getParent().resolve(devFileName);
     if (Files.isReadable(customPath)) {
       try (InputStream inputStream = Files.newInputStream(customPath)) {
         properties.load(inputStream);
@@ -32,6 +39,18 @@ public class PropertiesUtils {
     }
 
     return properties;
+  }
+
+  public static void loadFromFileWithOverrides(Path filePath, Consumer<Path> merger) throws IOException {
+    try (InputStream inputStream = Files.newInputStream(filePath)) {
+      merger.accept(filePath);
+    }
+    Path overridesDirPath = filePath.getParent().resolve(filePath.getFileName() + OVERRIDE_FOLDER_POSTFIX);
+    if (Files.exists(overridesDirPath) && Files.isDirectory(overridesDirPath)) {
+      try (Stream<Path> fileStream = Files.list(overridesDirPath).filter(Files::isRegularFile).sorted()) {
+        fileStream.forEachOrdered(merger);
+      }
+    }
   }
 
   public static void setSystemPropertyIfAbsent(final String name, final String value) {
--- nab-common/src/test/java/ru/hh/nab/common/properties/PropertiesUtilsTest.java
+++ nab-common/src/test/java/ru/hh/nab/common/properties/PropertiesUtilsTest.java
@@ -1,14 +1,16 @@
 package ru.hh.nab.common.properties;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Properties;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static ru.hh.nab.common.properties.PropertiesUtils.DEFAULT_DEV_FILE_EXT;
+import static ru.hh.nab.common.properties.PropertiesUtils.OVERRIDE_FOLDER_POSTFIX;
 import static ru.hh.nab.common.properties.PropertiesUtils.SETINGS_DIR_PROPERTY;
 import static ru.hh.nab.common.properties.PropertiesUtils.fromFilesInSettingsDir;
 
@@ -17,17 +19,27 @@ public class PropertiesUtilsTest {
   private static final String TEST_FILE_PREFIX = "settings.properties";
   private Path propertiesFile;
   private Path devPropertiesFile;
+  private Path overrideDir;
 
   @BeforeEach
   public void setUp() throws Exception {
     System.setProperty(SETINGS_DIR_PROPERTY, TMP_DIR);
     propertiesFile = Files.createTempFile(TEST_FILE_PREFIX, "");
-    devPropertiesFile = Files.createFile(Paths.get(propertiesFile.toString() + DEFAULT_DEV_FILE_EXT));
+    overrideDir = Files.createDirectory(
+      propertiesFile.getParent().resolve(propertiesFile.getFileName() + OVERRIDE_FOLDER_POSTFIX)
+    );
+    devPropertiesFile = Files.createFile(
+      propertiesFile.getParent().resolve(propertiesFile.getFileName() + DEFAULT_DEV_FILE_EXT)
+    );
   }
 
   @AfterEach
   public void tearDown() throws Exception {
     System.clearProperty(SETINGS_DIR_PROPERTY);
+    Files.walk(overrideDir)
+      .sorted(Comparator.reverseOrder())
+      .map(Path::toFile)
+      .forEach(File::delete);
     Files.deleteIfExists(propertiesFile);
     Files.deleteIfExists(devPropertiesFile);
   }
@@ -45,6 +57,19 @@ public class PropertiesUtilsTest {
   }
 
   @Test
+  public void fromFilesInSettingsDirShouldOverrideAlphabetically() throws Exception {
+    String testKey = "testProperty";
+    Files.write(propertiesFile, String.format("%s=%s", testKey, "123").getBytes());
+    Files.write(overrideDir.resolve("a.properties"), String.format("%s=%s", testKey, "456").getBytes());
+    String expectedValue = "789";
+    Files.write(overrideDir.resolve("b.properties"), String.format("%s=%s", testKey, expectedValue).getBytes());
+
+    Properties properties = fromFilesInSettingsDir(propertiesFile.getFileName().toString());
+    assertEquals(1, properties.size());
+    assertEquals(expectedValue, properties.getProperty(testKey));
+  }
+
+  @Test
   public void fromFilesInSettingsDirShouldOverrideDevProperties() throws Exception {
     String testKey = "testProperty";
     String testValue = "123";
@@ -59,17 +84,26 @@ public class PropertiesUtilsTest {
   }
 
   @Test
+  public void fromFilesInSettingsDirShouldOverrideAlphabeticallyButDevIsHighest() throws Exception {
+    String testKey = "testProperty";
+    Files.write(propertiesFile, String.format("%s=%s", testKey, "123").getBytes());
+    Files.write(overrideDir.resolve("a.properties"), String.format("%s=%s", testKey, "456").getBytes());
+    Files.write(overrideDir.resolve("b.properties"), String.format("%s=%s", testKey, "789").getBytes());
+    String expectedValue = "test";
+    Files.write(devPropertiesFile, String.format("%s=%s", testKey, "test").getBytes());
+
+    Properties properties = fromFilesInSettingsDir(propertiesFile.getFileName().toString());
+    assertEquals(1, properties.size());
+    assertEquals(expectedValue, properties.getProperty(testKey));
+  }
+
+  @Test
   public void testSetSystemPropertyIfAbsent() {
     String testValue = "tmp";
-
     System.clearProperty(SETINGS_DIR_PROPERTY);
-
     PropertiesUtils.setSystemPropertyIfAbsent(SETINGS_DIR_PROPERTY, testValue);
-
     assertEquals(testValue, System.getProperty(SETINGS_DIR_PROPERTY));
-
     PropertiesUtils.setSystemPropertyIfAbsent(SETINGS_DIR_PROPERTY, "some new value");
-
     assertEquals(testValue, System.getProperty(SETINGS_DIR_PROPERTY));
   }
 }

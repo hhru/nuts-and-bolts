c923d869233c229013b10c2a1acb17d9dac2092b
Vladimir Bazhmin <v.bazhmin@hh.ru>
2023-10-23 11:53:12 +0300

    HH-196375 : Microprofile annotations for ExceptionMapper

--- nab-starter/src/main/java/ru/hh/nab/starter/exceptions/AnyExceptionMapper.java
+++ nab-starter/src/main/java/ru/hh/nab/starter/exceptions/AnyExceptionMapper.java
@@ -4,15 +4,30 @@ import java.sql.SQLTransientConnectionException;
 import java.util.List;
 import java.util.concurrent.RejectedExecutionException;
 import javax.annotation.Priority;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 import static javax.ws.rs.core.Response.Status.SERVICE_UNAVAILABLE;
 import javax.ws.rs.ext.Provider;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import ru.hh.errors.common.Errors;
 import static ru.hh.nab.starter.exceptions.NabExceptionMapper.LOW_PRIORITY;
 
 @Provider
 @Priority(LOW_PRIORITY)
+@APIResponse(
+    responseCode = "500",
+    description = "Internal Server Error",
+    content = @Content(
+        mediaType = MediaType.APPLICATION_JSON,
+        schema = @Schema(
+            implementation = Errors.class
+        )
+    )
+)
 public class AnyExceptionMapper extends NabExceptionMapper<Exception> {
   public AnyExceptionMapper() {
     super(INTERNAL_SERVER_ERROR, LoggingLevel.ERROR_WITH_STACK_TRACE);
--- nab-starter/src/main/java/ru/hh/nab/starter/exceptions/IllegalArgumentExceptionMapper.java
+++ nab-starter/src/main/java/ru/hh/nab/starter/exceptions/IllegalArgumentExceptionMapper.java
@@ -1,12 +1,27 @@
 package ru.hh.nab.starter.exceptions;
 
 import javax.annotation.Priority;
+import javax.ws.rs.core.MediaType;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 import javax.ws.rs.ext.Provider;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import ru.hh.errors.common.Errors;
 import static ru.hh.nab.starter.exceptions.NabExceptionMapper.LOW_PRIORITY;
 
 @Provider
 @Priority(LOW_PRIORITY)
+@APIResponse(
+    responseCode = "400",
+    description = "Bad Request",
+    content = @Content(
+        mediaType = MediaType.APPLICATION_JSON,
+        schema = @Schema(
+            implementation = Errors.class
+        )
+    )
+)
 public class IllegalArgumentExceptionMapper extends NabExceptionMapper<IllegalArgumentException> {
   public IllegalArgumentExceptionMapper() {
     super(BAD_REQUEST, LoggingLevel.ERROR_WITH_STACK_TRACE);

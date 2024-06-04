dc92b8c2957a702ed8cf28f052e3dc28c95516bc
Igor Rogoza <i.rogoza@hh.ru>
2024-06-04 17:46:11 +0300

    HH-217505 set managed types to LocalContainerEntityManagerFactoryBean

--- nab-jpa/src/main/java/ru/hh/nab/jpa/NabJpaCommonConfig.java
+++ nab-jpa/src/main/java/ru/hh/nab/jpa/NabJpaCommonConfig.java
@@ -3,6 +3,8 @@ package ru.hh.nab.jpa;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.SharedEntityManagerCreator;
+import org.springframework.orm.jpa.persistenceunit.PersistenceManagedTypes;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -22,12 +25,26 @@ public class NabJpaCommonConfig {
       DataSource dataSource,
       JpaVendorAdapter jpaVendorAdapter,
       JpaPropertiesProvider jpaPropertiesProvider,
+      List<MappingConfig> mappingConfigs,
       Collection<EntityManagerFactoryCreationHandler> entityManagerFactoryCreationHandlers
   ) {
+    List<String> managedClassNames = mappingConfigs
+        .stream()
+        .map(MappingConfig::getAnnotatedClasses)
+        .flatMap(Stream::of)
+        .map(Class::getCanonicalName)
+        .toList();
+    List<String> managedPackages = mappingConfigs
+        .stream()
+        .map(MappingConfig::getPackagesToScan)
+        .flatMap(Stream::of)
+        .toList();
+
     NabEntityManagerFactoryBean entityManagerFactoryBean = new NabEntityManagerFactoryBean();
     entityManagerFactoryBean.setDataSource(dataSource);
     entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
     entityManagerFactoryBean.setJpaProperties(jpaPropertiesProvider.get());
+    entityManagerFactoryBean.setManagedTypes(PersistenceManagedTypes.of(managedClassNames, managedPackages));
     entityManagerFactoryBean.setEntityManagerFactoryCreationHandlers(entityManagerFactoryCreationHandlers);
     return entityManagerFactoryBean;
   }

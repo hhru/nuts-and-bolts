7e96d8192077e55762f839e985e70d15a8d4402c
Igor Rogoza <i.rogoza@hh.ru>
2024-06-10 12:02:02 +0300

    HH-217505 remove nab-hibernate module

--- nab-hibernate/src/main/java/ru/hh/nab/hibernate/transaction/ExecuteOnDataSourceWrappedException.java
+++
@@ -1,9 +0,0 @@
-package ru.hh.nab.hibernate.transaction;
-
-import static java.util.Objects.requireNonNull;
-
-final class ExecuteOnDataSourceWrappedException extends RuntimeException {
-  public ExecuteOnDataSourceWrappedException(Throwable cause) {
-    super("Checked exception from @ExecuteOnDataSource", requireNonNull(cause));
-  }
-}

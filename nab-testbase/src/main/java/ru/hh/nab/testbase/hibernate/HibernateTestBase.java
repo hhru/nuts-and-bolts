43c7adb1740071b36e1da037bdfbbbf05c419e92
Igor Rogoza <i.rogoza@hh.ru>
2024-07-01 18:29:35 +0300

    HH-218702 add HibernateTestBase

--- nab-testbase/src/main/java/ru/hh/nab/testbase/hibernate/HibernateTestBase.java
+++ nab-testbase/src/main/java/ru/hh/nab/testbase/hibernate/HibernateTestBase.java
@@ -1,13 +1,13 @@
-package ru.hh.nab.testbase.jpa;
+package ru.hh.nab.testbase.hibernate;
 
 import jakarta.inject.Inject;
-import jakarta.persistence.EntityManager;
+import org.hibernate.Session;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.hh.nab.testbase.transaction.TransactionTestBase;
 
 @ExtendWith(SpringExtension.class)
-public abstract class JpaTestBase extends TransactionTestBase {
+public abstract class HibernateTestBase extends TransactionTestBase {
   @Inject
-  protected EntityManager entityManager;
+  protected Session session;
 }

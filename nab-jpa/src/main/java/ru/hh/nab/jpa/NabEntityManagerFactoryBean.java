a9df535a2b177b0c985a118f02e525769c8e1ad5
Igor Rogoza <i.rogoza@hh.ru>
2024-07-16 20:29:31 +0300

    HH-222905 remove entities scanning from NabEntityManagerFactoryBean

--- nab-jpa/src/main/java/ru/hh/nab/jpa/NabEntityManagerFactoryBean.java
+++ nab-jpa/src/main/java/ru/hh/nab/jpa/NabEntityManagerFactoryBean.java
@@ -3,13 +3,8 @@ package ru.hh.nab.jpa;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.spi.PersistenceUnitInfo;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.persistenceunit.PersistenceManagedTypes;
-import org.springframework.orm.jpa.persistenceunit.PersistenceManagedTypesScanner;
 
 public class NabEntityManagerFactoryBean extends LocalContainerEntityManagerFactoryBean {
 
@@ -24,26 +19,9 @@ public class NabEntityManagerFactoryBean extends LocalContainerEntityManagerFact
     }
   }
 
-  @Override
-  public void setManagedTypes(PersistenceManagedTypes managedTypes) {
-    List<String> mergedManagedClassNames = new ArrayList<>(managedTypes.getManagedClassNames());
-
-    List<String> managedPackages = managedTypes.getManagedPackages();
-    if (!managedPackages.isEmpty()) {
-      PersistenceManagedTypes scannedManagedTypes = scanPackages(managedPackages);
-      mergedManagedClassNames.addAll(scannedManagedTypes.getManagedClassNames());
-    }
-    super.setManagedTypes(PersistenceManagedTypes.of(mergedManagedClassNames, managedPackages));
-  }
-
   public void setEntityManagerFactoryCreationHandlers(
       @Nullable Collection<EntityManagerFactoryCreationHandler> entityManagerFactoryCreationHandlers
   ) {
     this.entityManagerFactoryCreationHandlers = entityManagerFactoryCreationHandlers;
-  }
-
-  private PersistenceManagedTypes scanPackages(List<String> packagesToScan) {
-    PathMatchingResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
-    return new PersistenceManagedTypesScanner(resourcePatternResolver).scan(packagesToScan.toArray(new String[]{}));
   }
 }

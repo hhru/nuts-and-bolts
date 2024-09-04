634ed87ff996d1e357969d7b67df08a86738e4e4
Igor Rogoza <i.rogoza@hh.ru>
2024-09-04 19:25:31 +0300

    HH-229103 get properties from Environment

--- nab-web-spring-boot-starter/src/main/java/ru/hh/nab/sentry/SentryInitializer.java
+++ nab-web-spring-boot-starter/src/main/java/ru/hh/nab/sentry/SentryInitializer.java
@@ -6,24 +6,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationContextInitializedEvent;
 import org.springframework.context.ApplicationListener;
-import ru.hh.nab.common.properties.FileSettings;
+import org.springframework.core.env.Environment;
 
 public class SentryInitializer implements ApplicationListener<ApplicationContextInitializedEvent> {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SentryInitializer.class);
-  private static final String SENTRY_RELEASE_ENV = "SENTRY_RELEASE";
+  private static final String SENTRY_RELEASE_PROPERTY = "sentry.release";
+  private static final String SENTRY_DSN_PROPERTY = "sentry.dsn";
 
   @Override
   public void onApplicationEvent(ApplicationContextInitializedEvent event) {
-    FileSettings settings = event.getApplicationContext().getBean(FileSettings.class);
-    String dsn = settings.getString("sentry.dsn");
+    Environment environment = event.getApplicationContext().getEnvironment();
+    String dsn = environment.getProperty(SENTRY_DSN_PROPERTY);
     if (StringUtils.isBlank(dsn)) {
       LOGGER.warn("Sentry DSN is empty!");
     } else {
       Sentry.init(options -> {
         options.setEnableExternalConfiguration(true);
         options.setDsn(dsn);
-        options.setRelease(System.getProperty(SENTRY_RELEASE_ENV));
+        options.setRelease(environment.getProperty(SENTRY_RELEASE_PROPERTY));
       });
     }
   }

3e87b80841b7659319fe0d90b0346a9271590e53
Ilya Safonov <breakjessy@gmail.com>
2024-09-13 13:30:15 +0300

    HH-229373: remove high cardinality labels for jdbc metrics

--- nab-data-source/src/main/java/ru/hh/nab/datasource/monitoring/NabMetricsTrackerFactoryProvider.java
+++ nab-data-source/src/main/java/ru/hh/nab/datasource/monitoring/NabMetricsTrackerFactoryProvider.java
@@ -1,26 +1,19 @@
 package ru.hh.nab.datasource.monitoring;
 
 import ru.hh.nab.common.properties.FileSettings;
-import ru.hh.nab.datasource.monitoring.stack.CompressedStackFactoryConfig;
 import ru.hh.nab.metrics.StatsDSender;
 
 public class NabMetricsTrackerFactoryProvider implements MetricsTrackerFactoryProvider<NabMetricsTrackerFactory> {
   private final String serviceName;
   private final StatsDSender statsDSender;
-  private final CompressedStackFactoryConfig compressedStackFactoryConfig;
 
   public NabMetricsTrackerFactoryProvider(String serviceName, StatsDSender statsDSender) {
-    this(serviceName, statsDSender, new CompressedStackFactoryConfig());
-  }
-
-  public NabMetricsTrackerFactoryProvider(String serviceName, StatsDSender statsDSender, CompressedStackFactoryConfig compressedStackFactoryConfig) {
     this.serviceName = serviceName;
     this.statsDSender = statsDSender;
-    this.compressedStackFactoryConfig = compressedStackFactoryConfig;
   }
 
   @Override
   public NabMetricsTrackerFactory create(FileSettings dataSourceSettings) {
-    return new NabMetricsTrackerFactory(serviceName, statsDSender, compressedStackFactoryConfig, dataSourceSettings);
+    return new NabMetricsTrackerFactory(serviceName, statsDSender, dataSourceSettings);
   }
 }

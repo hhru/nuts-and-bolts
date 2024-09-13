67fd7cdc868c8f8254dec59b74960ea93d98b0ec
Igor Rogoza <i.rogoza@hh.ru>
2024-09-13 21:35:21 +0300

    HH-230067 remove jettyThreadPool bean from NabTestConfig

--- nab-testbase/src/main/java/ru/hh/nab/testbase/NabTestConfig.java
+++ nab-testbase/src/main/java/ru/hh/nab/testbase/NabTestConfig.java
@@ -2,16 +2,9 @@ package ru.hh.nab.testbase;
 
 import com.timgroup.statsd.NoOpStatsDClient;
 import com.timgroup.statsd.StatsDClient;
-import org.eclipse.jetty.util.thread.ThreadPool;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Scope;
-import ru.hh.nab.common.properties.FileSettings;
-import ru.hh.nab.metrics.StatsDSender;
-import static ru.hh.nab.starter.server.jetty.JettyServerFactory.createJettyThreadPool;
-import static ru.hh.nab.starter.server.jetty.JettySettingsConstants.JETTY;
 import ru.hh.nab.web.NabDeployInfoConfiguration;
 import ru.hh.nab.web.NabMetricsConfiguration;
 import ru.hh.nab.web.NabTaskSchedulingConfiguration;
@@ -24,12 +17,6 @@ import ru.hh.nab.web.NabTaskSchedulingConfiguration;
 })
 public class NabTestConfig {
   public static final String TEST_SERVICE_NAME = "testService";
-
-  @Bean
-  @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-  ThreadPool jettyThreadPool(FileSettings fileSettings, String serviceName, StatsDSender statsDSender) throws Exception {
-    return createJettyThreadPool(fileSettings.getSubSettings(JETTY), serviceName, statsDSender);
-  }
 
   @Bean
   StatsDClient statsDClient() {

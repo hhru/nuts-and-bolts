5c816473747f7218df73e413d533f997fa98a722
Igor Rogoza <i.rogoza@hh.ru>
2024-09-13 03:43:55 +0300

    HH-230067 copy scheduling beans

--- nab-web/src/main/java/ru/hh/nab/starter/NabCommonConfig.java
+++ nab-web/src/main/java/ru/hh/nab/starter/NabCommonConfig.java
@@ -1,17 +1,9 @@
 package ru.hh.nab.starter;
 
-import java.util.concurrent.ScheduledExecutorService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.hh.nab.common.executor.ScheduledExecutor;
 
 @Configuration
 public class NabCommonConfig {
 
   public static final String TEST_PROPERTIES_FILE_NAME = "service-test.properties";
-
-  @Bean
-  ScheduledExecutorService scheduledExecutorService() {
-    return new ScheduledExecutor();
-  }
 }

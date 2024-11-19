d8bee260d84cb8d50916fc2ec333cdb6e8db668d
Igor Rogoza <i.rogoza@hh.ru>
2024-11-19 20:08:30 +0300

    HH-238749 use SpringExtensionWithFailFast in JpaTestBase and TransactionTestBase

--- nab-testbase/src/main/java/ru/hh/nab/testbase/jpa/JpaTestBase.java
+++ nab-testbase/src/main/java/ru/hh/nab/testbase/jpa/JpaTestBase.java
@@ -2,11 +2,8 @@ package ru.hh.nab.testbase.jpa;
 
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.hh.nab.testbase.transaction.TransactionTestBase;
 
-@ExtendWith(SpringExtension.class)
 public abstract class JpaTestBase extends TransactionTestBase {
   @Inject
   protected EntityManager entityManager;

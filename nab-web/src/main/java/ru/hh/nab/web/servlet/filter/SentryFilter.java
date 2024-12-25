470df0081a99169adc1f96547d60a88120b4de70
Igor Rogoza <i.rogoza@hh.ru>
2024-12-25 14:50:44 +0300

    HH-243379 move RequestHeaders to nab-common

--- nab-web/src/main/java/ru/hh/nab/web/servlet/filter/SentryFilter.java
+++ nab-web/src/main/java/ru/hh/nab/web/servlet/filter/SentryFilter.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
-import ru.hh.nab.web.http.RequestHeaders;
+import ru.hh.nab.common.constants.RequestHeaders;
 
 public class SentryFilter extends OncePerRequestFilter {
 

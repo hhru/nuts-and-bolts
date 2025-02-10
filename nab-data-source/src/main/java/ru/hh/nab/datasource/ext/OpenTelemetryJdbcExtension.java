16041d185048d17a01482fa8ca35e1826b2553de
Pavel Orlov <p.orlov@hh.ru>
2025-02-10 14:45:18 +0300

    HH-247929 make OpenTelemetryJdbcExtension javadoc Spring agnostic

--- nab-data-source/src/main/java/ru/hh/nab/datasource/ext/OpenTelemetryJdbcExtension.java
+++ nab-data-source/src/main/java/ru/hh/nab/datasource/ext/OpenTelemetryJdbcExtension.java
@@ -3,7 +3,7 @@ package ru.hh.nab.datasource.ext;
 /**
  * <p>Extension provides the ability to decorate DataSource with wrapper sending OpenTelemetry spans.
  * This extension used if you create DataSource via {@link ru.hh.nab.datasource.DataSourceFactory}.
- * To activate the extension simply implement it as a Spring bean.<p/>
+ * Pass implementation to {@link ru.hh.nab.datasource.DataSourceFactory} as a parameter instead of <code>null</code> to activate it.<p/>
  *
  * <p>Default implementation placed in nab-telemetry-jdbc module.</p>
  */

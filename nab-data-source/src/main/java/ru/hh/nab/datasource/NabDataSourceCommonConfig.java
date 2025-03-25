ac48a673b7eb8f18772598ae0170258b1af1850c
Roman Kozlov <r.kozlov@hh.ru>
2025-03-25 18:32:32 +0300

    HH-253799 create only one DataSourceContextTransactionManager bean in context, remove `@Primary`

--- nab-data-source/src/main/java/ru/hh/nab/datasource/NabDataSourceCommonConfig.java
+++ nab-data-source/src/main/java/ru/hh/nab/datasource/NabDataSourceCommonConfig.java
@@ -3,13 +3,11 @@ package ru.hh.nab.datasource;
 import java.util.function.Function;
 import static java.util.stream.Collectors.toMap;
 import java.util.stream.Stream;
-import javax.sql.DataSource;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.support.JdbcTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import ru.hh.nab.common.properties.FileSettings;
 import ru.hh.nab.datasource.aspect.ExecuteOnDataSourceAspect;
@@ -40,13 +38,5 @@ public class NabDataSourceCommonConfig {
         .of(applicationContext.getBeanNamesForType(DataSourceContextTransactionManager.class))
         .collect(toMap(Function.identity(), beanName -> applicationContext.getBean(beanName, DataSourceContextTransactionManager.class)));
     return new ExecuteOnDataSourceAspect(applicationContext.getBean(DataSourceContextTransactionManager.class), txManagers);
-  }
-
-  @Bean
-  DataSourceContextTransactionManager defaultJdbcTransactionManager(DataSource dataSource) {
-    JdbcTransactionManager jdbcTransactionManager = new JdbcTransactionManager(dataSource);
-    jdbcTransactionManager.setLazyInit(false);
-    jdbcTransactionManager.afterPropertiesSet();
-    return new DataSourceContextTransactionManager(jdbcTransactionManager);
   }
 }

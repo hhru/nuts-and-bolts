797d54e82c97f739e2ec5f371c8cae9f542872d1
Igor Rogoza <i.rogoza@hh.ru>
2025-05-30 17:19:36 +0300

    HH-262517 clear trace id in SentryFilter after request

---
+++ nab-starter/src/main/java/ru/hh/nab/starter/sentry/SentryScopeConfigurator.java
@@ -0,0 +1,31 @@
+package ru.hh.nab.starter.sentry;
+
+import io.sentry.Sentry;
+import io.sentry.protocol.SentryId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SentryScopeConfigurator {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(SentryScopeConfigurator.class);
+
+  @SuppressWarnings("UnstableApiUsage")
+  public static void setTraceId(String traceId) {
+    Sentry.configureScope(scope -> {
+      try {
+        scope.getPropagationContext().setTraceId(new SentryId(traceId));
+      } catch (RuntimeException e) {
+        // TODO: it's better to use warn/error log level, but there are too much invalid rids.
+        //  Fix log level to warn/error after https://jira.hh.ru/browse/PORTFOLIO-19764
+        LOGGER.debug("Unable to set sentry trace id: {}", traceId, e);
+      }
+    });
+  }
+
+  public static void clearTraceId() {
+    Sentry.configureScope(scope -> scope.getPropagationContext().setTraceId(SentryId.EMPTY_ID));
+  }
+
+  private SentryScopeConfigurator() {
+  }
+}

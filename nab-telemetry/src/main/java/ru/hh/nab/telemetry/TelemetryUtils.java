f8a20b4255293838b0506820a9c1d7622b9a04a8
Igor Rogoza <i.rogoza@hh.ru>
2026-05-12 19:02:18 +0300

    fix telemetry imports

--- nab-telemetry/src/main/java/ru/hh/nab/telemetry/TelemetryUtils.java
+++ nab-telemetry/src/main/java/ru/hh/nab/telemetry/TelemetryUtils.java
@@ -2,12 +2,13 @@ package ru.hh.nab.telemetry;
 
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Span;
-import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.EXCEPTION_EVENT_NAME;
-import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.EXCEPTION_MESSAGE;
-import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.EXCEPTION_TYPE;
+import static io.opentelemetry.semconv.ExceptionAttributes.EXCEPTION_MESSAGE;
+import static io.opentelemetry.semconv.ExceptionAttributes.EXCEPTION_TYPE;
 import static java.util.Optional.ofNullable;
 
 public final class TelemetryUtils {
+
+  private static final String EXCEPTION_EVENT_NAME = "exception";
 
   public static void addExceptionEventToSpan(Span span, Throwable throwable) {
     span.addEvent(
